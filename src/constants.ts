0ebba8ebb868a70810e627e94ebf6df62c9ed62d
Erik Escoffier <me@erik.nz>
2023-05-30 17:53:59 +0200

    Allow food group selection

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,5 @@
 import { Category } from "./types";
 
-export const CATEGORY_COLORS: Record<Category, string> = {
-  vegetables: "#0FB5AE",
-  nuts: "#4046CA",
-  grain: "#F68511",
-  fruits: "#DE3D82",
-  tubbers: "#7E84FA",
-};
-
 export const CATEGORIES: Category[] = [
   "grain",
   "nuts",
@@ -15,3 +7,29 @@ export const CATEGORIES: Category[] = [
   "fruits",
   "tubbers",
 ];
+
+export const CATEGORIES_PROPS: Record<
+  Category,
+  { name: string; color: string }
+> = {
+  vegetables: {
+    name: "Vegetables",
+    color: "#0FB5AE",
+  },
+  nuts: {
+    name: "Nuts",
+    color: "#4046CA",
+  },
+  grain: {
+    name: "Grain",
+    color: "#F68511",
+  },
+  fruits: {
+    name: "Fruits",
+    color: "#DE3D82",
+  },
+  tubbers: {
+    name: "Tubers",
+    color: "#7E84FA",
+  },
+};
