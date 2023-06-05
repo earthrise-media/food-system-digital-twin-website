925d3d3f084a41cda96cc644ea08aeac8c63a20b
Vitor George <vitor.george@gmail.com>
2023-06-05 14:36:34 +0100

    Connect inbound/outbound routes to seeded data

--- src/pages/api/county/[countyId]/outbound.js
+++ src/pages/api/county/[countyId]/outbound.js
@@ -4,9 +4,9 @@ export default async function handler(req, res) {
   const { countyId } = req.query;
 
   // Get inbound kcal flows for target county
-  const inbound = await db("kcal_flows")
+  const outbound = await db("kcal_flows")
     .select(
-      "kcal_flows.origin_id as county_id",
+      "kcal_flows.destination_id as county_id",
       db.raw("counties.properties->>'name' as county_name"),
       db.raw("ST_AsGeoJSON(ST_Centroid(counties.geom)) as county_centroid"),
       "kcal_flows.crop_id as crop_id",
@@ -14,13 +14,13 @@ export default async function handler(req, res) {
       "crops.category as crop_category",
       "kcal_flows.value"
     )
-    .where("kcal_flows.destination_id", countyId)
-    .join("counties", "kcal_flows.origin_id", "=", "counties.id")
+    .where("kcal_flows.origin_id", countyId)
+    .join("counties", "kcal_flows.destination_id", "=", "counties.id")
     .join("crops", "kcal_flows.crop_id", "=", "crops.id")
     .orderBy("kcal_flows.value", "desc");
 
   return res.status(200).json({
-    inbound: inbound.map((f) => ({
+    outbound: outbound.map((f) => ({
       ...f,
       county_centroid: JSON.parse(f.county_centroid),
     })),
