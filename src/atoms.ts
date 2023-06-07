7237de58e486377dd77928c2aca3c5be2df65e71
Erik Escoffier <me@erik.nz>
2023-06-07 18:21:41 +0200

    Change default view

--- src/atoms.ts
+++ src/atoms.ts
@@ -6,10 +6,10 @@ import { County, FlowType, Category } from "./types";
 export const countiesAtom = atom<FeatureCollection<Geometry, County> | null>(
   null
 );
-export const countyAtom = atomWithHash<string>("county", "47173");
+export const countyAtom = atomWithHash<string>("county", "21137");
 export const countyHighlightedAtom = atom<string | null>(null);
 export const searchAtom = atomWithHash<boolean>("search", false);
-export const flowTypeAtom = atomWithHash<FlowType>("flowType", "producer");
+export const flowTypeAtom = atomWithHash<FlowType>("flowType", "consumer");
 export const foodGroupAtom = atomWithHash<Category | null>("foodGroup", null);
 
 export const selectedCountyAtom = atom((get) => {
