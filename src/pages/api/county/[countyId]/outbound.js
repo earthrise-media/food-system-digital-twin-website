796c9db8a0d741630516032e101184b879621327
Erik Escoffier <me@erik.nz>
2023-07-20 21:20:11 +0200

    Simulate the simulation

--- src/pages/api/county/[countyId]/outbound.js
+++ src/pages/api/county/[countyId]/outbound.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
       "kcal_flows.crop_id as crop_id",
       "crops.name as crop_name",
       "crops.category as crop_category",
-      "kcal_flows.value"
+      "kcal_flows.value"  
     )
     .where("kcal_flows.origin_id", countyId)
     .join("counties", "kcal_flows.destination_id", "=", "counties.id")
@@ -27,13 +27,20 @@ export default async function handler(req, res) {
 
   const routes = await getSymmetricRoutes(countyId, destinationIds);
 
+  // TODO replace with real values
+  const outboundsWithAdverseConditions = outbound.map((f) => ({
+    ...f,
+    value_drought:  Math.round(f.value * (0.5 + 0.5 * Math.random())),
+    value_heat_stress:  Math.round(f.value * (0.5 + 0.5 * Math.random()))
+  }));
+
   return res.status(200).json({
-    outbound: groupFlowsByCounty(outbound).map((f) => ({
+    outbound: groupFlowsByCounty(outboundsWithAdverseConditions).map((f) => ({
       ...f,
       county_centroid: JSON.parse(f.county_centroid),
       route_geometry: routes.find((r) => r.destination_id === f.county_id)
         ?.polyline,
     })),
-    stats: getStats(outbound),
+    stats: getStats(outboundsWithAdverseConditions),
   });
 }
