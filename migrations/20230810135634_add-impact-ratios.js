f344640e18ec9a55062a4fc3fa120c5b90aa7915
Vitor George <vitor.george@gmail.com>
2023-08-10 16:10:33 +0100

    Remove comment

--- migrations/20230810135634_add-impact-ratios.js
+++ migrations/20230810135634_add-impact-ratios.js
@@ -3,7 +3,6 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  // alter table kcal_flows add column impact_ratios jsonb;
   return knex.schema.alterTable("kcal_flows", function (t) {
     t.jsonb("impact_ratios");
   });
