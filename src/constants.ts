25d1b0b291a2d01b8d7e7336dbe6c72db83baa96
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2023-10-04 17:36:47 -0700

    updating stress conditions

--- src/constants.ts
+++ src/constants.ts
@@ -51,13 +51,13 @@ export const ADVERSE_CONDITIONS_OPTIONS: {
     value: "drought",
     label: "Drought",
     tilesUrl:
-      "https://food-system-digital-twin-public.s3.us-east-2.amazonaws.com/drought-tiles/{z}/{x}/{y}.png",
+      "https://food-system-digital-twin-public.s3.us-east-2.amazonaws.com/drought-tiles-new/{z}/{x}/{y}.png",
   },
   {
     value: "heatStress",
     label: "Heat stress",
     tilesUrl:
-      "https://food-system-digital-twin-public.s3.us-east-2.amazonaws.com/heat-tiles/{z}/{x}/{y}.png",
+      "https://food-system-digital-twin-public.s3.us-east-2.amazonaws.com/heat-tiles-new/{z}/{x}/{y}.png",
   },
 ];
 
